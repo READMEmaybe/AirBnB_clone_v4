2ae8996bcbdc76b1204fb7ee05cc7e371f765ea3
READMEmaybe <haroune11411@gmail.com>
2024-01-13 00:16:24 +0100

    Fix task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,3 +1,13 @@
+$('document').ready(function () {
+	$('input[type=checkbox]').click(function () {
+	  const checkedAmenities = {};
+	  $('input[type=checkbox]:checked').each(function () {
+		checkedAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
+	  });
+	  $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
+	});
+  });
+  
 $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
   if (data.status === 'OK') {
 	$('#api_status').addClass('available');
